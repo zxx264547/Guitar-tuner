b2d6f2acc12ea9ca2e988d99dc814586456e7e4d
zxx264547 <79500289+zxx264547@users.noreply.github.com>
2025-12-28 10:57:44 +0800

    Delete app/build directory

--- app/build/intermediates/cxx/Debug/5731445b/logs/arm64-v8a/build_command_tuner.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"F:\\SDK\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "F:\\AndroidStudioProjects\\Guitar-tuner\\app\\.cxx\\Debug\\5731445b\\arm64-v8a" ^
-  tuner
--- app/build/intermediates/cxx/Debug/5731445b/logs/x86/build_command_tuner.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"F:\\SDK\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "F:\\AndroidStudioProjects\\Guitar-tuner\\app\\.cxx\\Debug\\5731445b\\x86" ^
-  tuner
