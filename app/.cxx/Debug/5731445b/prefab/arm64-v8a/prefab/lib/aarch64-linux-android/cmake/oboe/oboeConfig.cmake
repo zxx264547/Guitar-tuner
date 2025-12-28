2fe7fc24ed3b909aef3503ef1eba87da92c6b5e3
ZhangXiaoxiao <2645471525@qq.com>
2025-12-28 10:49:03 +0800

    修改文件
    Signed-off-by: ZhangXiaoxiao <2645471525@qq.com>

--- app/.cxx/Debug/5731445b/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/oboe/oboeConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET oboe::oboe)
-add_library(oboe::oboe SHARED IMPORTED)
-set_target_properties(oboe::oboe PROPERTIES
-    IMPORTED_LOCATION "C:/Users/Administrator/.gradle/caches/8.9/transforms/57d1efb4069e1c11f03fe5f35f5a9859/transformed/oboe-1.7.0/prefab/modules/oboe/libs/android.arm64-v8a/liboboe.so"
-    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/Administrator/.gradle/caches/8.9/transforms/57d1efb4069e1c11f03fe5f35f5a9859/transformed/oboe-1.7.0/prefab/modules/oboe/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
